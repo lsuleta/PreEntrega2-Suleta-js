a9146ec22e6a5e091aeae298ba4d3b2f6333c510
Suleta Lucas <lsuleta60@gmail.com>
2023-02-13 22:12:36 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// let hora1 = 1200
+// let hora2 = 1500
+// let hora3 = 1800
+// let te = 200
+// let cafe = 150
+// let exprimido = 250
+
+
+// let nombre = prompt("Ingrese su nombre")
+// console.log(nombre);
+// alert (`Bienvenido/a ${nombre}`)
+
+// alert (`Para ingresar al lugar solo con reserva y los montos son para 1 hora: ${hora1}, para 2 horas ${hora2} y para 3 horas ${hora3}`)
+// let horas = parseInt (prompt("¿Cuantas horas desea reservar?"))
+// console.log(horas);
+
+// while (horas < 1 || horas > 3) {
+//     alert("No ingreso una cantidad de horas válidas, vuelva a intentarlo")
+//     parseInt(prompt("Ingrese una cantidad de horas válidas"))
+// }
+
+// if (horas === 1){ 
+//     alert(`Usted deberá abonar $${hora1}`)
+//     horas = hora1
+// } else if (horas === 2){  
+//     alert(`Usted deberá abonar $${hora2}`)
+//     horas = hora2
+// } else if (horas === 3) {
+//     alert(`Usted deberá abonar $${hora3}`)
+//     horas = hora3
+// }
+
+// let infusion = parseInt (prompt ("¿Que infusión desea consumir?, las opciones son 1) té, 2) café y 3) exprimido"))
+// console.log(infusion);
+
+// while (infusion < 1 || infusion > 3) {
+//     alert ("No ingreso una infusión válida, intente nuevamente")
+//     prompt ("¿Desea 1) té, 2) café o 3) exprimido?")
+// }
+
+// if (infusion === 1) {
+//     alert(`Usted deberá abonar $${te}`)
+//     infusion = te
+// } else if (infusion === 2) {
+//     alert(`Usted deberá abonar $${cafe}`)
+//     infusion = cafe
+// } else if (infusion === 3) {
+//     alert(`Usted deberá abonar $${exprimido}`)
+//     infusion = exprimido
+// }
+
+// function sumarMonto(horas, infusion) {
+//     return horas + infusion;
+// }
+
+// let resultado = sumarMonto(horas, infusion)
+// console.log(resultado);
+// alert(`El total a pagar es: $${resultado}`)
+
